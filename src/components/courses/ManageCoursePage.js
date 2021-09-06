96c2e769799f7ad3e747c50e8f8f7741710bed0c
dunson-orange <mpdunson@gmail.com>
2021-09-06 17:55:04 -0400

    11-clip12

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
@@ -11,6 +11,7 @@ function ManageCoursePage({
   authors,
   loadAuthors,
   loadCourses,
+  saveCourse,
   ...props
 }) {
   const [course, setCourse] = useState({ ...props.course });
@@ -33,8 +34,14 @@ function ManageCoursePage({
   function handleChange(event) {
     const { name, value } = event.target;
     setCourse((prevCourse) => ({
+      ...prevCourse,
       [name]: name === "authorId" ? parseInt(value, 10) : value,
     }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course);
   }
 
   return (
@@ -43,6 +50,7 @@ function ManageCoursePage({
       errors={errors}
       authors={authors}
       onChange={handleChange}
+      onSave={handleSave}
     />
   );
 }
@@ -53,6 +61,7 @@ ManageCoursePage.propTypes = {
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -66,6 +75,7 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
   loadCourses,
   loadAuthors,
+  saveCourse,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
